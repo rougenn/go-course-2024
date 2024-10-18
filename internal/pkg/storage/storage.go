115c03a9d2e793715bd79b5ccb45167c70d7b7c5
rougenn <deziq.hi@gmail.com>
2024-10-18 19:55:21 +0300

    small fixes

--- internal/pkg/storage/storage.go
+++ internal/pkg/storage/storage.go
@@ -1,6 +1,9 @@
 package storage
 
 import (
+	"errors"
+	"slices"
+	"sort"
 	"strconv"
 
 	"go.uber.org/zap"
@@ -10,9 +13,9 @@ import (
 type kind string
 
 type val struct {
-	k kind
-	s string
-	n int
+	value_type   kind
+	string_value string
+	int_value    int
 }
 
 const (
@@ -23,12 +26,14 @@ const (
 
 type Storage struct {
 	inner  map[string]*val
+	arrays map[string][]int
 	logger *zap.Logger
 }
 
 func NewStorage() *Storage {
 	// to turn off the logger while benchmarks
 	logger, _ := zap.NewProduction(zap.IncreaseLevel(zapcore.DPanicLevel))
+
 	// logger, _ := zap.NewProduction()
 
 	// defer logger.Sync()
@@ -38,6 +43,7 @@ func NewStorage() *Storage {
 	return &Storage{
 		inner:  make(map[string]*val),
 		logger: logger,
+		arrays: make(map[string][]int),
 	}
 }
 
@@ -47,8 +53,8 @@ func (r *Storage) Set(key, input_val string) {
 	int_val, err := strconv.Atoi(input_val)
 	if err == nil {
 		r.inner[key] = &val{
-			k: KindInt,
-			n: int_val,
+			value_type: KindInt,
+			int_value:  int_val,
 		}
 		r.logger.Info("key obtained", zap.String("key", key),
 			zap.Int("val", int_val), zap.String("type", string(KindInt)))
@@ -56,55 +62,105 @@ func (r *Storage) Set(key, input_val string) {
 		return
 	}
 	r.inner[key] = &val{
-		k: KindString,
-		s: input_val,
+		value_type:   KindString,
+		string_value: input_val,
 	}
 	r.logger.Info("key obtained", zap.String("key", key),
 		zap.String("val", input_val),
 		zap.String("type", string(KindString)))
 }
 
-func (r *Storage) GetValue(key string) (*val, bool) {
+var (
+	ErrNoValue = errors.New("key value doesnt exist")
+)
+
+func (r *Storage) GetValue(key string) (*val, error) {
 	// defer r.logger.Sync()
 
 	val, ok := r.inner[key]
 	if !ok {
-		r.logger.Info("key value doesnt exists", zap.String("key", key))
-		return nil, ok
+		r.logger.Info("key value doesnt exist", zap.String("key", key))
+		return nil, ErrNoValue
 	}
-	if val.k == KindString {
+	if val.value_type == KindString {
 		r.logger.Info("storage request", zap.String("key", key),
-			zap.String("val", val.s), zap.String("type", string(val.k)))
+			zap.String("val", val.string_value), zap.String("type", string(val.value_type)))
 	} else {
 		r.logger.Info("storage request", zap.String("key", key),
-			zap.Int("val", val.n), zap.String("type", string(val.k)))
+			zap.Int("val", val.int_value), zap.String("type", string(val.value_type)))
 	}
 
-	return val, ok
+	return val, nil
 }
 
-func (r *Storage) Get(key string) *string {
+func (r *Storage) Get(key string) (string, error) {
 	// defer r.logger.Sync()
 
 	val, ok := r.GetValue(key)
-	if !ok {
-		return nil
+	if ok != nil {
+		return "", ok
 	}
-	switch val.k {
+	switch val.value_type {
 	case KindString:
-		return &val.s
+		return val.string_value, nil
 	case KindInt:
-		output := strconv.Itoa(val.n)
-		return &output
+		output := strconv.Itoa(val.int_value)
+		return output, nil
 	default:
-		return nil
+		return "", nil
 	}
 }
 
-func (r *Storage) GetKind(key string) *kind {
-	val, ok := r.GetValue(key)
-	if !ok {
-		return nil
+func (r *Storage) GetKind(key string) (kind, error) {
+	val, err := r.GetValue(key)
+	if err != nil {
+		return "", err
 	}
-	return &val.k
+	return val.value_type, err
 }
+
+func (r *Storage) Rpush(key string, arr ...int) {
+
+	r.arrays[key] = append(r.arrays[key], arr...)
+
+	r.logger.Info("New elems added to RIGHT side of slice",
+		zap.Int("count of elems", len(arr)), zap.String("key", key))
+}
+
+func (r *Storage) Lpush(key string, arr ...int) {
+
+	r.arrays[key] = append(arr, r.arrays[key]...)
+
+	r.logger.Info("New elems added to LEFT side of slice",
+		zap.Int("count of elems", len(arr)), zap.String("key", key))
+}
+
+func (r *Storage) Raddtoset(key string, arr ...int) {
+	to_append := []int{}
+	sort.Slice(to_append, func(i, j int) bool {
+		return i < j
+	})
+
+	prev := to_append[len(to_append)-1] + 1
+
+	for _, elem := range arr {
+		if prev == elem {
+			continue
+		}
+		if ind := slices.Index(r.arrays[key], elem); ind == -1 {
+			to_append = append(to_append, elem)
+		}
+		prev = elem
+	}
+
+	r.arrays[key] = append(r.arrays[key], to_append...)
+}
+
+func (r *Storage) Lpop(args ...int) {
+	switch len(args) {
+	case 1:
+
+	}
+}
+
+func (r *Storage) Rpop(args ...int)

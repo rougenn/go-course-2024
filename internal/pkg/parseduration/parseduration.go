f8d4e6c73b283f6a0c05bbd1e3a71e37d8fe6bb4
rougenn <deziq.hi@gmail.com>
2024-11-08 15:19:03 +0300

    hw8 done

--- internal/pkg/parseduration/parseduration.go
+++ internal/pkg/parseduration/parseduration.go
@@ -1,4 +1,4 @@
-package argsparser
+package parseduration
 
 import (
 	"fmt"
@@ -6,39 +6,44 @@ import (
 	"strconv"
 )
 
-func ParseArgs() (int, int, string, string) {
+const (
+	defaultSaveDuration  = 60
+	defaultFilename      = "my-storage.json"
+	defaultPort          = "8090"
+	defaultClearDuration = 60
+)
+
+func ParseDuration() (int, int, string, string) {
 	saveDuration, ok := os.LookupEnv("SAVE_DURATION")
 	if !ok {
 		fmt.Println("save dur is not provided")
-		saveDuration = "60"
 	}
 	clearDuration, ok := os.LookupEnv("CLEAR_DURATION")
 	if !ok {
 		fmt.Println("clear dur is not provided")
-		saveDuration = "60"
 	}
 	filename, ok := os.LookupEnv("STORAGE_FILENAME")
 	if !ok {
 		fmt.Println("save dir is not provided")
-		filename = "my-storage.json"
+		filename = defaultFilename
 	}
 	port, ok := os.LookupEnv("SERVER_PORT")
 	if !ok {
 		fmt.Println("port is not provided")
-		port = "8090"
+		port = defaultPort
 	}
 
 	SD, err := strconv.Atoi(saveDuration)
 	if err != nil {
 		fmt.Println("incorrect format of saveDuration, set to default")
-		SD = 60
+		SD = defaultSaveDuration
 	}
 
 	CD, err := strconv.Atoi(clearDuration)
 
 	if err != nil {
 		fmt.Println("incorrect format of saveDuration, set to default")
-		CD = 60
+		CD = defaultClearDuration
 	}
 
 	return SD, CD, filename, ":" + port
